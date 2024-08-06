135882f372a3d84a8510c82f2cd812abaec253f9
BrianSechelo <sechbrian123@gmail.com>
2024-08-06 13:58:18 +0000

    Task 0

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" Module Documentation """
+
+
+def minOperations(n):
+    """Function Documentation"""
+    if n < 2:
+        return 0
+    operations = 0
+    factor = 2
+    while n > 1:
+        while n % factor == 0:
+            operations += factor
+            n /= factor
+        factor += 1
+    return operations
