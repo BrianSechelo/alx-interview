7e4253f8146e3b9e956cca409cdd896dc4d2137d
BrianSechelo <sechbrian123@gmail.com>
2024-08-20 21:46:54 +0000

    UTF-8 Validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module doumentation"""
+def validUTF8(data):
+    """Func documentation"""
+    num_bytes = 0
+    for num in data:
+        if num_bytes == 0:
+            if (num >> 5) == 0b110:
+                num_bytes = 1
+            elif (num >> 4) == 0b1110:
+                num_bytes = 2
+            elif (num >> 3) == 0b11110:
+                num_bytes = 3
+            elif (num >> 7):
+                return False
+        else:
+            if (num >> 6) != 0b10:
+                return False
+            num_bytes -= 1
+    return num_bytes == 0
