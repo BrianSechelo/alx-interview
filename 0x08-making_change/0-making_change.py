833b7923d4c1f5da339299b24ef56f471cd8c14c
BrianSechelo <sechbrian123@gmail.com>
2024-09-18 22:48:33 +0000

    Task 0

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -2,24 +2,21 @@
 """
 Minimum Number of Coins using while loops
 """
+
+
 def makeChange(coins, total):
     """ fewest number of coins needed to meet total """
     if total <= 0:
         return 0
-
     coins.sort(reverse=True)
-
     num_coins = 0
     i = 0
     n = len(coins)
-
     while i < n and total > 0:
         while total >= coins[i]:
             total -= coins[i]
             num_coins += 1
-        i += 1
-
-    
+        i += 1    
     if total == 0:
         return num_coins
     else:
